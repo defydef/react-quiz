5d8e2e90084ef2660e7cb268c4aad486fd0a1292
defy <8841170+defydef@users.noreply.github.com>
2023-08-24 07:45:32 +0700

    Dispatch data when getting questions from fake API

--- src/App.js
+++ src/App.js
@@ -1,16 +1,37 @@
-import { useEffect } from "react";
+import { useEffect, useReducer } from "react";
 import Header from "./Header";
 import Main from "./Main";
 
+const initialState = [
+  {
+    question: "",
+    options: [],
+    // loading, error, ready, active, finished
+    status: "loading",
+  },
+];
+
+function reducer(state, action) {
+  // console.log(action);
+  switch (action.type) {
+    case "dataReceived":
+      return action.payload;
+    default:
+      throw new Error("Undefined action");
+  }
+}
+
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  console.log(state);
+
   useEffect(function () {
     async function fetchQuestions() {
       const res = await fetch("http://localhost:8000/questions");
-      console.log(res);
       if (!res.ok) throw new Error();
       const data = await res.json();
       if (data.Response === "False") throw new Error(data.Error);
-      console.log(data);
+      dispatch({ type: "dataReceived", payload: data });
     }
     fetchQuestions();
   }, []);
@@ -20,7 +41,7 @@ export default function App() {
       <Header />
       <Main>
         <p>1/15</p>
-        <p>Questions</p>
+        <p>{state[0].question}</p>
       </Main>
     </div>
   );
