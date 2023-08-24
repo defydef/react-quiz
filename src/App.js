20efc933c4dbfdb7ed0939e30b8fbe37f4cbc8a6
defy <8841170+defydef@users.noreply.github.com>
2023-08-24 07:27:43 +0700

    Create skeleton of React Quiz app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
-import DateCounter from "./DateCounter";
+import { useEffect } from "react";
+import Header from "./Header";
+import Main from "./Main";
 
 export default function App() {
+  useEffect(function () {
+    async function fetchQuestions() {
+      const res = await fetch("http://localhost:8000/questions");
+      console.log(res);
+      if (!res.ok) throw new Error();
+      const data = await res.json();
+      if (data.Response === "False") throw new Error(data.Error);
+      console.log(data);
+    }
+    fetchQuestions();
+  }, []);
+
   return (
     <div className="app">
-      <DateCounter />
+      <Header />
+      <Main>
+        <p>1/15</p>
+        <p>Questions</p>
+      </Main>
     </div>
   );
 }
